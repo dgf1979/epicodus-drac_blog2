95003d344cee86c4097e34972b9acfdfb5909b78
Drew Finstrom & Jessica Engel <student@epicodus.com>
2015-06-10 09:52:12 -0700

    add new post and delete new post; retain posts in local data store

--- controllers/post.js
+++ controllers/post.js
@@ -7,5 +7,11 @@ Blogger.PostController = Ember.ObjectController.extend({
     save: function() {
       this.set('isEditing', false);
     },
+    delete: function(){
+      if(confirm('Are you sure?')){
+        this.get('model').destroyRecord();
+        this.transitionToRoute('posts');
+      };
+    }
   }
 });
